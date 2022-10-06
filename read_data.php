f08bdfaaf33cf2c598ef64ecc8551431e5827e79
tarekassioui-git <107467534+tarekassioui-git@users.noreply.github.com>
2022-10-06 12:16:15 +0200

    hg

--- read_data.php
+++ read_data.php
@@ -8,41 +8,56 @@
 
         $parsed_registration = Array();
 
+        /* Lettura targa */
         $parsed_registration['targa'] = $content->identifier;
+
+        /* Lettura telaio */
         $parsed_registration['telaio'] = $content->providerData->providerGroups[0]->entries[0]->value;
+
+        /* Lettura marca */ 
         $parsed_registration['marca'] = $content->make->name;
+
+        /* Lettura modello */
         $parsed_registration['modello'] = $content->model->name;
+
+        /* Lettura versione divisione di stringa versioni e separazione della versione dal suo codice*/ 
         $parsed_registration['versione'] = explode(',', $content->providerData->providerGroups[0]->entries[1]->value);
-        
         for ($i=0 ; $i < sizeof($parsed_registration['versione']); $i++)
         {
+            /* Separazione versione dal codice */ 
             $temp = explode('-', $parsed_registration['versione'][$i]);
-            $temp[0] = preg_replace( $pattern, $replace,$temp[0]);
+
+            /*Rimozione spazi vuoti*/ 
+            $temp[0] = preg_replace( $pattern, $replace, $temp[0]);
             $parsed_registration['versione'][$i] = array('versione' => $temp[1], 'codice' => $temp[0]);
         }
 
+        /*Lettura cambio*/ 
         $parsed_registration['cambio'] = $content->transmission->description[0]->value;
         if(!isset($parsed_registration['cambio']))
             $parsed_registration['cambio']= $content->providerData->providerGroups[2]->entries[6]->value;
 
+
+        /* Lettura immatricolazione */
         $parsed_registration['immatricolazione'] = $content->regDate->name;
-        
+
+        /* Lettura carburante */
         $parsed_registration['carburante'] = $content->engine->description[0]->value; 
         if(!isset($parsed_registration['carburante']))
             $parsed_registration['carburante']= $content->providerData->providerGroups[2]->entries[9]->value;
 
+        /* Lettura potenza */ 
         $parsed_registration['potenza'] = $content->engine->description[4]->value . 'KW - ' . $content->engine->description[2]->value . 'Cv';
         
+        /* Lettura trazione */
         $parsed_registration['trazione'] = $content->wheelDrive->description[0]->value;
         if(!isset($parsed_registration['trazione']))
             $parsed_registration['trazione']= $content->providerData->providerGroups[2]->entries[5]->value;
 
-        
-        var_dump($parsed_registration);
-
         /* Popolamento form */ 
         fill_registrationData($parsed_registration);
         
+        /* Se almeno uno dei dati non è stato letto correttamente la vettura non viene salvata sul database*/
         foreach($parsed_registration as $value)
         {
             if(!isset($value))
