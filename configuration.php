177903e94f95d466bf337d2b4b78fe55bb70c8e7
tarekassioui-git <107467534+tarekassioui-git@users.noreply.github.com>
2022-10-10 11:34:22 +0200

    asd

--- configuration.php
+++ configuration.php
@@ -54,6 +54,8 @@
 
             
         $current_page = rgpost('gform_source_page_number_53') ? rgpost('gform_source_page_number_53') : 1;
+
+        echo $current_page;
         if ($current_page == 1) {
                  
             $checked = check_db($plate);
