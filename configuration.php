2ebf068e4db8da67cab7d507e3a38108f511a1fd
tarekassioui-git <107467534+tarekassioui-git@users.noreply.github.com>
2022-10-11 13:35:12 +0200

    asf

--- configuration.php
+++ configuration.php
@@ -28,7 +28,31 @@
 
 
 
-
+    /* <?php
+    ?>
+            <script type="text/javascript">
+                jQuery(document).on("gform_page_loaded", function(event, form_id, current_page){
+    
+                var km = document.getElementById("input_53_13").value;
+    
+                if(current_page == 2)
+                {        
+                    console.log("PROVO CHIAMATA");
+                    
+                    <?php 
+                        call_valuation_api();
+                    ?>
+    
+                    console.log("CHIAMATA EFFETTUATA")
+                }
+    
+                
+            });
+            </script>
+            <?php
+    
+    
+    ?> */
 
 
 
